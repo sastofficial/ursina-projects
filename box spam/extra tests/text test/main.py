2390781e7b672699d152df62f0a7a720dc9384ec
sastofficial <39431922+sastofficial@users.noreply.github.com>
2022-03-16 17:23:19 +0100

    text thing cool

--- box spam/extra tests/text test/main.py
+++ box spam/extra tests/text test/main.py
@@ -1,24 +1,16 @@
+# following this tutorial
+# https://www.ursinaengine.org/cheat_sheet.html#text
 from ursina import *
-  app = Ursina()
-  descr = dedent('''
-      <scale:1.5><orange>Rainstorm<default><scale:1>
-      Summon a <azure>rain storm <default>to deal 5 <azure>water
+app = Ursina()
+text_string = input("Input text: ")
+Text.default_resolution = 1080 * Text.size
+text_stuff = Text(text=text_string, origin=(0,0), backround=True)
 
-      damage <default>to <hsb(0,1,.7)>everyone, <default><image:file_icon> <red><image:file_icon> test <default>including <orange>yourself. <default>
-      Lasts for 4 rounds.''').strip()
-
-  Text.default_resolution = 1080 * Text.size
-  test = Text(text=descr, origin=(0,0), background=True)
-
-
-
-
-  def input(key):
-      if key == 'a':
-          print('a')
-          test.text = '<default><image:file_icon> <red><image:file_icon> test '
-          print('by', test.text)
-
-  window.fps_counter.enabled = False
-  print('....', Text.get_width('yolo'))
-  app.run()
+def input(key):
+    if key == 'a':
+        print("a")
+        text_stuff.text = '<default><image:file_icon> <red><image:file_icon> test '
+        print('by', text_stuff.text)
+window.fps_counter.enabled = False
+print('....', Text.get_width('yes'))
+app.run()
